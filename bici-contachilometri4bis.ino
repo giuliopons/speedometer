82fe92935e582ba48011e5f900613269c9e71d7c
Giulio Pons <pons@rockit.it>
2024-10-27 10:56:35 +0100

    Smoothly move between numbers in km/h speed

--- bici-contachilometri4bis.ino
+++ bici-contachilometri4bis.ino
@@ -42,13 +42,15 @@ void ledprint(char* str, Adafruit_AlphaNum4 *alphanum, int startpos) {
   alphanum->writeDisplay();
 }
 
+const byte IS_PRESSED = 0;
+const byte IS_NOT_PRESSED = 1;
 
 int buttonPin1 = 7;
 int buttonPin2 = 5;
 
 int reedSensor = 2;  // reed sensor
-int count = 0;       // counter of wheel loops
-byte bv = 2;         // prev button value
+int count = 0;       // counter of wheel loops in time deltat seconds
+byte button1_status = IS_NOT_PRESSED; // button status
 
 volatile int flag = 0;  // used in interrupt
 
@@ -65,9 +67,9 @@ float r = (0.711/2.0) ;       // wheel radius in m
 float p = 2.0*r*3.1415;       // wheel circumference in m
 int deltat = 2; //sec         // time window of refresh of the counter
 
-unsigned long w = 0;          // timer for velocity from previous speed to the current one
-unsigned long wa = 0;         // timer for visualizzare passaggio magnete
-unsigned long wp = 0;         // timer for button1
+unsigned long w = 0;          // timer for velocity display from previous speed to the current one
+unsigned long t_mag = 0;      // timer to show the magnet passage
+unsigned long t_but1 = 0;     // timer for button1
 unsigned long timepass;       // timer for tempo
 
 int function = 0;             // 0 =show km/h   1= show km     2= show giri    3 = show elapsed time
@@ -96,38 +98,28 @@ void setup() {
   // attach an interrupt to the change of the digital pin of the reed sensor
   // everytime the reed changes the interrupt function is called
   attachInterrupt(digitalPinToInterrupt(reedSensor), magnetPassage, CHANGE);
-  Serial.println("attachInterrupt");  
   timepass = millis();
 }
 
 
 // function called every time the reed sensor value changes (the magnet has passed)
 void magnetPassage(){
-  Serial.println("OK");
   byte a=digitalRead(reedSensor);
-  if(a==0) flag++;
+  
+  if(a==0 && flag==0) { flag++; Serial.print(a); }
   EIFR = 0x01;    // EIFR (External Interrupt Flag Register) reset interrupt?
 }
 
 
-
-/*
-
- aggiungere 1 bottone funzione
- che cambia il dato mostrato nel display
- 1 prima pressione : switch a tempo passato hh.mm
- 2 seconda pressione: switch a km percorsi
- 0 terza pressione: switch a velocità
- pressione lunga: azzera contatore tempo e km
- 
- * 
- */
 
 float val0=0;
 float val1=0;
 float inc;
 unsigned long sec;
 bool asteriskmode;
+
+
+
 
 void loop() {
 
@@ -136,30 +128,26 @@ void loop() {
   String s0="";
   String s="";
 
+  
 
-
-  // ogni deltat secondi
+  // every deltat seconds
   while(t>millis()) {
 
-    // controllo la pressione del bottone
-    // e cambio la modalità
-    // (aggiungere modalità velocità media e velocità massima)
-
+    // check buttons and perform actions
     // --------------------------------------------------------------
     // button
-    int b = digitalRead(buttonPin1);
-    b = b==1 ? 0 : 1 ;
-    //Serial.println("b:"+(String)b+" bv:"+String(bv));
-    if(b==1 && bv ==2) {
+    byte b = digitalRead(buttonPin1);    // 0 pressed, 1 not pressed
+    
+    if(b == IS_PRESSED && button1_status == IS_NOT_PRESSED) {
       //
       // button has been pressed
-      bv = 1;
+      button1_status = IS_PRESSED;
     }
-    if(b==0 && bv==1) {
+    if(b == IS_NOT_PRESSED && button1_status == IS_PRESSED) {
       // button has been released 
       function++;
       if (function>3) function = 0;
-      Serial.println("Function is " + (String)function);
+      
 
       // display changed function
       String f = "";
@@ -169,98 +157,75 @@ void loop() {
       if (function == 3) f="time";
       ledprint(f.c_str(),&alpha4,0);
 
-      wp = millis() + 1000;
+      t_but1 = millis() + 1000; // display is showing something for a second
 
-      bv = 2;
+      button1_status = IS_NOT_PRESSED;
     }
-    if(bv==2 && millis()>wp && wp > 0) {
+    if(button1_status==IS_NOT_PRESSED && millis()>t_but1 && t_but1 > 0) {
       // terminate showing changed function
       alpha4.clear(); 
-      //alpha4.writeDisplay();
-      Serial.println("cleared");
 
-      wp = 0;
-      //bv = 2; // ready to work
+
     }
     // --------------------------------------------------------------
 
-    delay(50);
+    // delay(50);
 
 
-
-    v1 = v1;      // velocità istantanea calcolata ogni deltat secondi
-    m = p * spin; // distanza in m
-    spin = spin;  // giri da interrupt
+    // status:
+    spin = spin;  // spins of the wheel from interrupt
+    v1 = v1;      // speed calculated every deltat
+    m = p * spin; // distance in meters
     sec = ( millis() - timepass ) / 1000; // seconds from beginning
     
 
-    if (function == 0) {
+    // functions
+
+    if (function == 0) {    // km/h mode
       val1 = v1;
-      inc = (v1-v0) / 5.0;
+      inc = (v1-v0) / 10.0;  // gradually change from one speed to next in 5 steps
       val0 = val0 + inc;
       asteriskmode = true;
     }
-    if (function==1 && bv==2) {
+    if (function==1 && button1_status==IS_NOT_PRESSED) {
       val1 = m;
       asteriskmode = false;
     }
-    if (function==2 && bv==2) {
+    if (function==2 && button1_status==IS_NOT_PRESSED) {
       val1 = spin;
       asteriskmode = false;
     }
-    if (function==3 && bv==2) {
+    if (function==3 && button1_status==IS_NOT_PRESSED) {
       val1 = sec;
       asteriskmode = false;
     }
    
  
-
-
-    if(false && millis() > w ) {
-                                
-      w =millis() + 200; // next time
-
-      // stampa il nuovo val1
-    
-    }
-
  
     if (function==0) {
-Serial.println("xxx "+(String)w);
-            // if enaugh time has passed print speed, show variation gradually
-            if(millis() > w ) {
-                            
-              w =millis() + 200; // next timer
-        
-              v0 = v0 + inc; // add increment
-        
-              s = (String)round(v0);
-              Serial.println(s);
-              if(v0 < 100) s = " "+s; // left pad     
-              if(v0 < 10) s = " "+s;  // left pad     
-
-        
-              if(s0!=s) {
-                //alpha4.clear();
-                if(bv==2 && wp==0) {
-                  alpha4.writeDigitAscii(1,' '); alpha4.writeDigitAscii(2,' '); alpha4.writeDigitAscii(3,' ');
-                  ledprint(s.c_str(),&alpha4,1);        
-                }
-                s0=s;
-              }
-        
+        // if enaugh time has passed print speed, show variation gradually
+        if(millis() > w ) {
+          w =millis() + 20; // next timer
+          //v0 = v0 + inc; // add increment
+          if( ( v1 > v0 && v0+inc < v1) || (v1 < v0 && v0 + inc > 0) ) v0 = v0+inc;
+            
+          s = (String)round(v0);
+          if(v0 < 100) s = " "+s; // left pad     
+          if(v0 < 10) s = " "+s;  // left pad     
+          if(s0!=s) {
+            //alpha4.clear();
+            if(button1_status==IS_NOT_PRESSED && t_but1==0) {
+              alpha4.writeDigitAscii(1,' '); alpha4.writeDigitAscii(2,' '); alpha4.writeDigitAscii(3,' ');
+              ledprint(s.c_str(),&alpha4,1);        
             }
-
-
-     
-
-
-
+            s0=s;
+          }        
+        }
     }
 
 
     // m e km
-    if (function==1 && bv==2) {
+    if (function==1 && button1_status==IS_NOT_PRESSED) {
 
         String u = "m";
 
@@ -280,62 +245,57 @@ Serial.println("xxx "+(String)w);
 
         u = sm + u;
         
-        if(  wp==0 ) ledprint(u.c_str(),&alpha4,0);        
+        if(  t_but1==0 ) ledprint(u.c_str(),&alpha4,0);        
         
     }
 
 
 
-    if (function==2 && bv==2  && wp==0) {
+    if (function==2 && button1_status==IS_NOT_PRESSED  && t_but1 == 0) {
         ledprint(String(spin).c_str(),&alpha4,0);        
         
     }
 
 
-    if (function==3 && bv==2) {
-        //int minu = s / 60;   // 2
-        //int seco = s - 60*minu;   //1
+    if (function==3 && button1_status==IS_NOT_PRESSED) {
         
-        int ore = (sec / 60) / 60;   // 0
-        int minu = (sec / 60) % 60;
-        int seco = sec % 60;
+        int hours = (sec / 60) / 60;
+        int minutes = (sec / 60) % 60;
+        int seconds = sec % 60;
 
         char ss[2];
-        sprintf(ss,"%02d", seco);
+        sprintf(ss,"%02d", seconds);
         char mm[2];
-        sprintf(mm,"%02d", minu);
+        sprintf(mm,"%02d", minutes);
         char oo[2];
-        sprintf(oo,"%02d", ore);
+        sprintf(oo,"%02d", hours);
 
-        if( wp==0) {
-          if (ore > 0) {
+        if( t_but1 == 0) {
+          if (hours > 0) {
+            // write hours and minutes
             ledprint(strcat(strcat(oo,"."),mm),&alpha4,0);        
           } else {
+            // write minutes and seconds
             ledprint(strcat(strcat(mm,"."),ss),&alpha4,0);        
           }      
         }
     }
-    
-
-
-
-    
 
     if(flag>0) {
-            if(function==0) {
-                if(bv==2 && wp==0) {
-                  alpha4.writeDigitAscii(0,'*');alpha4.writeDisplay(); // show magnet passage
-                  Serial.println("*");
-                }
-                wa =millis() + 100; // next timer
-            }
+      // interrupt has been called, I have to update counters and show magnet passage
+      if(function==0) {
+          if(button1_status==IS_NOT_PRESSED && t_but1==0) {
+            alpha4.writeDigitAscii(0,'*');alpha4.writeDisplay(); // show magnet passage
+          }
+          t_mag =millis() + 100; // next timer
+      }
       flag=0;
-      count++;
-      spin++;
-    }
+      count++;    // counter for spins in deltat seconds
+      spin++;     // total count of spins
+    } 
 
-    if(function==0 && bv==2) {
-      if (millis()>wa  && wp==0) {
+    if(function==0 && button1_status==IS_NOT_PRESSED) {
+      if (millis()>t_mag  && t_but1==0) {
         alpha4.writeDigitAscii(0,' ');alpha4.writeDisplay(); 
       } // clear show magnet passage
     }
@@ -348,7 +308,7 @@ Serial.println("xxx "+(String)w);
    v0=v1;
    // calculate new speed (km/h) (kph)
    v1 = round ( (count*p / deltat) * 3.6 * 10.0 ) / 10.0;
-    
+   Serial.println("\nv1:" + (String)v1 + " count:"+(String)count);
    count=0;
 
 }
